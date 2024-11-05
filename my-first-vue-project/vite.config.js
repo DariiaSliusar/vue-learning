b77c55da698430aa1101b865f275f841e10de04d
Dariia Sliusar <dariiasliusar@gmail.com>
2024-11-05 10:16:37 +0100

    from mixins to composables

--- my-first-vue-project/vite.config.js
+++ my-first-vue-project/vite.config.js
@@ -7,12 +7,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      reactivityTransform: true,
+    }),
     vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
